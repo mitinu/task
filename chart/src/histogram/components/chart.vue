2e3182bb75154e7a5d17834c44c3925bb26d21da
mitin <mitinu2006@gmai.com>
2025-09-04 11:51:37 +0700

    commit

--- chart/src/histogram/components/chart.vue
+++ chart/src/histogram/components/chart.vue
@@ -5,15 +5,7 @@
         <columnNumbering
             :height="height"
         />
-    <div 
-    class="chart" 
-    >
-        <columnNumbering
-            :height="height"
-        />
         <columnsMain
-            :height="height"
-            :dataChart="dataChart"
             :height="height"
             :dataChart="dataChart"
         />
@@ -26,9 +18,6 @@ import columnNumbering from './columnNumbering.vue';
 
 export default {
     name: "chart",
-    props:{
-        dataChart:{required: true}
-    },
     props:{
         dataChart:{required: true}
     },
@@ -52,28 +41,12 @@ export default {
             }
         }
         this.height = (parseInt((maxNumber+"").slice(0,1))+1)*10**(Math.floor(Math.log10(Math.abs(maxNumber))))
-        console.log(this.height)
-            height: null
-        }
-    },
-    beforeMount(){
-        let maxNumber = 0
-        for (const indicatorsKey in this.dataChart) {
-           const indicators = this.dataChart[indicatorsKey]
-           for (const indicatorKey in indicators) {
-                const indicator = indicators[indicatorKey]
-                if(maxNumber<indicator.value)
-                    maxNumber = indicator.value
-            }
-        }
-        this.height = (parseInt((maxNumber+"").slice(0,1))+1)*10**(Math.floor(Math.log10(Math.abs(maxNumber))))
     }
 }
 </script>
 
 <style>
 .chart{
-    flex: 1;
     flex: 1;
     display: flex;
 }
